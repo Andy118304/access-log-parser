935fb7f6011c40c98c6c1ad76290b73b69a5ace2
Andrey Podkhvatilin <APodkhvatilin@inno.tech>
2025-02-06 16:55:00 +0300

    Удалены функции, которые были в программе ранее. Добавлен ввод с консоли, сложение, вычитание, умножение и частное вводимых чисел Исправлены ошибки

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println ("Введите число:");
+        System.out.println ("Введите первое число:");
         int firstNumber = new Scanner(System.in).nextInt();
+                System.out.println ("Введите второе число:");
         int secondNumber = new Scanner(System.in).nextInt();
         double quotient = (double) firstNumber/secondNumber;
-        System.out.println (firstNumber-secondNumber);
-        System.out.println (firstNumber+secondNumber);
-        System.out.println (firstNumber*secondNumber);
-        System.out.println (quotient);
+        System.out.println ("Разность чисел:"+(firstNumber-secondNumber));
+        System.out.println ("Сумма чисел:"+(firstNumber+secondNumber));
+        System.out.println ("Произведение чисел:"+(firstNumber*secondNumber));
+        System.out.println ("Частное чисел:"+quotient);
     }
 }
