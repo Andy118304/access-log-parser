4055a4fb49521bd4958e6ed021a940d5f7acc69f
Andrey Podkhvatilin <APodkhvatilin@inno.tech>
2025-02-06 16:46:14 +0300

    Удалены функции, которые были в программе ранее. Добавлен ввод с консоли, сложение, вычитание, умножение и частное вводимых чисел

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Случайное число от 0 до 1: " +
-                Math.random());
+        System.out.println ("Введите число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        int secondNumber = new Scanner(System.in).nextInt();
+        double quotient = (double) firstNumber/secondNumber;
+        System.out.println (firstNumber-secondNumber);
+        System.out.println (firstNumber+secondNumber);
+        System.out.println (firstNumber*secondNumber);
+        System.out.println (quotient);
     }
 }
