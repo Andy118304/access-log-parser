62fc1540f530e4e933a74587f858be85ba9c6f3e
Andrey Podkhvatilin <APodkhvatilin@inno.tech>
2025-03-23 16:20:52 +0300

    Переделано. Вставлен метод чтения файла

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 import java.util.Scanner;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        boolean bool=false;
-        while (bool!=true) {
-            String path = new Scanner(System.in).nextLine();
+        int count = 0;//счетчик верных файлов
+        while (true) {
+            System.out.println("Путь файла:");
+            String path = new Scanner(System.in).nextLine();//сканер с ввода и объявление переменной path
+            //код, который определяет существует ли файл, к которому указан путь или нет
+            File file = new File(path);
+            boolean fileExist = file.exists(); //переменная будет равна true, если файл существует и false, если не существует
+
+            boolean isDirectory = file.isDirectory();
+                if (fileExist == false || isDirectory == false) {
+                    System.out.println("Указанный файл не существует или указанный путь является путём к папке, а не файлу");
+                    continue; //пропускаем оставшуюся часть цикла
+                } else if (fileExist == true && isDirectory == true) {
+                    count++;
+                    System.out.println("Путь указан верно.Это файл номер"+count);
+
+                }
         }
 
 
