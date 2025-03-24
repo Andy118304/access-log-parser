6e4ce8ecf821999e3ba59d77304014ee62d17a08
Andrey Podkhvatilin <APodkhvatilin@inno.tech>
2025-03-24 20:19:17 +0300

    Вставлен алгоритм для чтения файла. Считаются строки. Выбрасывается исключение при большом количестве символов

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Scanner;
 import java.io.File;
 
@@ -12,14 +14,43 @@ public class Main {
             boolean fileExist = file.exists(); //переменная будет равна true, если файл существует и false, если не существует
 
             boolean isDirectory = file.isDirectory();
-                if (fileExist == false || isDirectory == false) {
-                    System.out.println("Указанный файл не существует или указанный путь является путём к папке, а не файлу");
-                    continue; //пропускаем оставшуюся часть цикла
-                } else if (fileExist == true && isDirectory == true) {
-                    count++;
-                    System.out.println("Путь указан верно.Это файл номер"+count);
+            if (fileExist == false || isDirectory == false) {
+                System.out.println("Указанный файл не существует или указанный путь является путём к папке, а не файлу");
+                continue; //пропускаем оставшуюся часть цикла
+            } else if (fileExist == true && isDirectory == true) {
+                count++;
+                System.out.println("Путь указан верно.Это файл номер" + count);
 
+            }
+
+            int totalLines = 0;  //переменная - количество строк в файле
+            int maxLength = Integer.MAX_VALUE; // переменная - длина самой длинной строки в файле
+            int minLength = Integer.MIN_VALUE;// переменная - длина самой короткой строки в файле
+            //исключение, условие в задании
+            try {
+                //код, который будет построчно читать указанный файл
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+                    totalLines++; //считаем строки
+                    if (length > 1024) {
+                        throw new LineTooLongException("Строка длинной более 1024 символа"); //выбрасывание исключения при превышении 1024
+                    }
+                    if (length > maxLength) maxLength = length; //подсчет максимальной строки
+                    if (length > minLength) minLength = length;//подсчет минимальной строки
                 }
+                //вывод сообщений
+                System.out.println("Общее количество строк" + totalLines);
+                System.out.println("Самая длинная строка" + maxLength);
+                System.out.println("Самая короткая строка" + minLength);
+            } catch (LineTooLongException e) {
+                System.out.println("Ошибка программы. Слишком длинная строка в файле");
+                break;
+            } catch (Exception ex) {
+                System.out.println("Другие ошибки программы.Обратитесь к разработчику при этой ошибке");
+            }
         }
 
 
